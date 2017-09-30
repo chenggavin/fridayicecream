58b46e7b1758ee9878176c6761da5fccf2903514
chenggavin <gavincheng01@gmail.com>
2017-09-30 12:25:40 -0400

    leaned down code

--- app/main.js
+++ app/main.js
@@ -1,121 +1,20 @@
 var title = document.getElementById('title');
 
 
-
-// function boringButton() {
-//     if (myDiv.style.display === 'none') {
-//         myDiv.style.display = 'block';
-//     } else {
-//         myDiv.style.display = 'none';
-//     }
-
-// }
-
-
-
-function myChoc() {
-    if (chocDesc.style.display === 'none') {
-        chocDesc.style.display = 'block';
-    } else {
-        chocDesc.style.display = 'none';
-    }
-};
-function myChocA() {
-    if (chocDescA.style.display === 'none') {
-        chocDescA.style.display = 'block';
-    } else {
-        chocDescA.style.display = 'none';
-    }
+function buildMenu (columnName, className) {
+    var menuType = products[columnName];
+    var menuItem;
+    for (var i = 0; i < menuType.length; i++) {
+        menuItem = document.getElementById(className + (i + 1));
+        menuItem.innerHTML = 
+        '<span class="' + className + '-name col-xs-12">' + menuType[i].name + '</span>' +
+        '<span class="' + className + '-description col-xs-12">' + menuType[i].description + '</span>' +
+        '<span class="' + className + '-price col-xs-12">' + menuType[i].price + '</span>';
+    };
 };
 
-
-// regf.innerHTML = 
-
-
-
-var regular = products["Regular Flavors"];
-var regularFlavor;
-console.log("Regular: ", regular)
-for (i = 0; i <= regular.length; i++) {
-    regularFlavor = document.getElementById('reg-flavors' + (i + 1))
-    regularFlavor.innerHTML = 
-    '<span class="reg-flavors-name col-xs-12">' + regular[i].name + '</span>' + 
-    '<span class="reg-flavors-description col-xs-12">' + regular[i].description + '</span>' + 
-    '<span class="reg-flavors-price col-xs-12">' + regular[i].price + '</span>';
-};
-
-   console.log(regular[0]);
-
-
-     var regular1 = regular[i];
-
-  function regFinder (){
-
-
-
-
-for (i = 0; i < regular.length; i++) {
-            for (j = 0; j < regular[i].length; j++) {
-                reg.innerHTML = "regular[i][0]"
-
-            };
-        }}
-
-
-
-//chocDesc.innerHTML = regular[0].description + " " + regular[0].price
-
-
-
-
-// var obj = {a: 1, b: 2, c: 3};
-    
-
-
-// var reg = document.getElementById("reg");
-
-// reg.innerHTML = regular[0].name;
-
-
-
-
-// console.log(Object.keys(products));
-
-
-// for (i=0; i < regular[i].length; i++) {
-//  console.log(regular1[i])
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+buildMenu("Regular Flavors", "reg-flavors");
+buildMenu("Seasonal Flavors", "season-flavors");
+buildMenu("Serving Options", "cup-cone");
+buildMenu("Toppings", "sugar-type");
 
---
+++ app/oldcode.js
@@ -0,0 +1,91 @@
+
+
+// var regular = products["Regular Flavors"];
+// var regularFlavor;
+// for (i = 0; i < regular.length; i++) {
+//     regularFlavor = document.getElementById('reg-flavors' + (i + 1));
+//     regularFlavor.innerHTML =
+//     '<span class="reg-flavors-name col-xs-12">' + regular[i].name + '</span>' + 
+//     '<span class="reg-flavors-description col-xs-12">' + regular[i].description + '</span>' + 
+//     '<span class="reg-flavors-price col-xs-12">' + regular[i].price + '</span>';
+// };
+
+
+// var seasonal = products["Seasonal Flavors"];
+// var seasonalFlavor;
+// for(j = 0; j< seasonal.length; j++) {
+//     seasonalFlavor = document.getElementById("season-flavors" + (j + 1));
+//     seasonalFlavor.innerHTML = 
+//     '<span class="season-flavors-name col-xs-12">' + seasonal[j].name + '</span>' +
+//     '<span class="season-flavors-description col-xs-12">' + seasonal[j].description + '</span>' +
+//     '<span class="season-flavors-price col-xs-12">' + seasonal[j].price + '</span>';
+// };
+
+// var cupcone = products["Serving Options"]
+// var servingOptions = 
+// for(k=)
+
+// poplate column with name description and price of catagory passed in. 
+
+
+
+
+
+
+// function boringButton() {
+//     if (myDiv.style.display === 'none') {
+//         myDiv.style.display = 'block';
+//     } else {
+//         myDiv.style.display = 'none';
+//     }
+
+// }
+
+
+
+// function myChoc() {
+//     if (chocDesc.style.display === 'none') {
+//         chocDesc.style.display = 'block';
+//     } else {
+//         chocDesc.style.display = 'none';
+//     }
+// };
+// function myChocA() {
+//     if (chocDescA.style.display === 'none') {
+//         chocDescA.style.display = 'block';
+//     } else {
+//         chocDescA.style.display = 'none';
+//     }
+// };
+
+
+// regf.innerHTML = 
+
+
+
+
+//chocDesc.innerHTML = regular[0].description + " " + regular[0].price
+
+
+
+
+// var obj = {a: 1, b: 2, c: 3};
+    
+
+
+// var reg = document.getElementById("reg");
+
+// reg.innerHTML = regular[0].name;
+
+
+
+
+// console.log(Object.keys(products));
+
+
+// for (i=0; i < regular[i].length; i++) {
+//  console.log(regular1[i])
+// };
+
+
+
